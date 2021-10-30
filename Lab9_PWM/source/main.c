9cf6b34127289c940ed6bad36358750f299c5476
Tyler Lafond <tlafo001@wch136-18.cs.ucr.edu>
2021-10-30 12:15:06 -0700

    Created project

---
+++ Lab9_PWM/source/main.c
@@ -0,0 +1,64 @@
+/*	Author: tlafo001
+ *  Partner(s) Name: 
+ *	Lab Section: 022
+ *	Assignment: Lab # 9  Exercise # 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+// 0.954 hz is lowest frequency possible with this function,
+// based on settings in PWM_on()
+// Passing in 0 as the frequency will stop the speaker from generating sound
+void set_PWM(double frequency) {
+	static double current_frequency; // Keeps track of the current set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
+
+		// prevents OCR3A from overflowing, using prescalar 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+
+		// prevents OCR3A from underflowing, using prescalar 64
+		// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+
+		// set OCR3A based on desired frequency
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT3 = 0; // resets counter
+		current_frequency = frequency; // Updates the current_frequency
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+		// COM3A0: Toggle PB3 on compare match between counter and OCR3A
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+		// WGM32: When counter (TCNT3) matches OCR3A, reset counter
+		// CS31 & CS30: Set a prescalar of 64
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
